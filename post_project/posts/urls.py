c9745d9f07a6c48ed153d137b61eae7e0f12d51e
Lam Tieu <lm436150@dal.ca>
2024-12-08 23:46:56 -0400

    Add posts list navigation and single post page using slug

--- post_project/posts/urls.py
+++ post_project/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app = "posts"
+app_name = "posts"
 
 urlpatterns = [
-    path("", views.posts_list)
+    path("", views.posts_list, name='list'),
+    path("<slug:slug>", views.post_page, name='page'),
 ]
--- post_project/posts/views.py
+++ post_project/posts/views.py
@@ -6,5 +6,6 @@ def posts_list(request):
     posts = Post.objects.all().order_by('date')
     return render(request, 'posts/posts_list.html', { 'posts': posts })
 
-def post_page(request):
-    return render(request, 'posts/post_page.html')
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', { 'post': post })
