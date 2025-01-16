7928dad6b4de5fe8db05d63a92d6e1465fe14abc
Lam Tieu <tieunhatlam1210@gmail.com>
2025-01-15 23:27:59 -0400

    Add login feature for users

--- post_project/users/views.py
+++ post_project/users/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def user_registration(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
 
     return render(request, 'users/registration_page.html', { "form": form })
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'users/login_page.html', { "form": form })
